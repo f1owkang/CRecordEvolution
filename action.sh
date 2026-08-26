92d4dadfa52d41558ebb88f718ff493063c4a042
f1owkang <27006147+f1owkang@users.noreply.github.com>
2026-08-26 17:30:44 +0800

    add: shell 引导切换至 batteryd 二进制

--- action.sh
+++ action.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 DIR=${0%/*}
-if sh "$DIR/service.sh" once; then
+if "$DIR/bin/batteryd" once; then
 	echo "电池健康数据已刷新"
 else
-	echo "刷新失败，请查看 /sdcard/Documents/电池健康度.log"
+	echo "刷新失败，请查看模块 data/battery.db 的 events 表"
 fi
